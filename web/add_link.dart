1ad08193514eb4aa297b6d33594c18f527b57cf8
Neuro17 <biagio.chirico.86@gmail.com>
2013-02-06 11:59:23 +0100

    replace  method event with stream

--- web/add_link.dart
+++ web/add_link.dart
@@ -9,8 +9,8 @@ class AddLink extends WebComponent {
 
   void addTodo(Event e) {
     e.preventDefault(); // don't submit the form
-    var input = query('#new-todo');
-    var titolo = query('#new-titolo');
+    InputElement input = query('#new-todo');
+    InputElement titolo = query('#new-titolo');
     if ((input.value == '')||(titolo.value == '')) return;
     app.links.add(new Link(input.value, titolo.value));
     input.value = '';
